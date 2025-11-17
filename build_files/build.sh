377adb3212f84ba28be3ba403c6144d0c0daf134
Abno85 <abno85@users.noreply.github.com>
2025-11-17 11:30:46 +0100

    install coolercontrol via dnf

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-#dnf5 install -y tmux
+dnf5 install -y liquidctl coolercontrol
 dnf5 remove -y waydroid
-
-ujust install-coolercontrol
 
 # Use a COPR Example:
 #
